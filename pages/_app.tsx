79f49dc8436447a403f8a45fdbdee00d2d889f04
Franklyn Eduardo Medina <fbarrera@shokworks.io>
2022-11-18 17:18:50 -0400

    agregando tags open graph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,27 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#2e2e2e" />
+        <meta name="title" content="Dr. Daniel Urbano" />
+        <meta
+          name="description"
+          content="Especialista en Cirugía de Cerebro y Columna Vertebral"
+        />
+        <meta
+          name="keywords"
+          content="Dr,Daniel,Urbano,Daniel Urbano,Dr Daniel Urbano,Neurocirugia,Neurocirujano,San Cristobal,Doctor,Doctor San Cristobal,Neurocirujano en San Cristobal, Doctor Daniel Urbano"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="Spanish" />
+
+        <meta property="og:title" content="Dr. Daniel Urbano" />
+        <meta property="og:site_name" content="Dr. Daniel Urbano" />
+        <meta
+          property="og:description"
+          content="Especialista en Cirugía de Cerebro y Columna Vertebral"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/images/daniel-consultorio.jpg" />
       </Head>
       <Component {...pageProps} />
       <Footer />
